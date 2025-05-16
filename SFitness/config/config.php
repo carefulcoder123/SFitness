cd08f9dd6278601fb732afed4267d1edc99d3fe1
carefulcoder123 <sankalppawar2805@gmail.com>
2025-05-17 00:29:55 +0530

    Delete SFitness directory

--- SFitness/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gym');
-
-$conncection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-?>
